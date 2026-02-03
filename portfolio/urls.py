3f7c58aceb4c8c6ab26a2050213ce0d1c4286bfa
kutologojm <kutologomashego@gmail.com>
2026-02-04 00:47:10 +0800

    Added images for tools and programming languages in projects.html. Added additional boilerplate to pages html files and completed template for about-me.html and projects.html

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from portfolio import views
 
+app_name = 'portfolio'
 urlpatterns = [
-    path(
-        '',
-        views.Homepage.as_view(),
-        name='home',
-    )
+    path('', views.Homepage.as_view(), name='homepage'),
+    path('about-me/', views.AboutMe.as_view(), name='about-me'),
+    path('resume/', views.Resume.as_view(), name='resume'),
+    path('projects/', views.Projects.as_view(), name='projects'),
+    path('blog/', views.Blog.as_view(), name='blog'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -5,3 +5,19 @@ from django.views.generic import TemplateView
 # Create your views here.
 class Homepage(TemplateView):
     template_name = 'pages/homepage.html'
+
+
+class AboutMe(TemplateView):
+    template_name = 'pages/about-me.html'
+
+
+class Resume(TemplateView):
+    template_name = 'pages/resume.html'
+
+
+class Projects(TemplateView):
+    template_name = 'pages/projects.html'
+
+
+class Blog(TemplateView):
+    template_name = 'pages/blog.html'
